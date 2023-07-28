5e20ad97ef440e4033734165e20de9538336f594
Сapy <gutnikmi@mail.ru>
2023-07-28 22:21:14 +0300

    new stuff

---
+++ func.h
@@ -0,0 +1,5 @@
+long int add (int, int);
+long int sub (int, int);
+long int mult(int, int);
+long int pow1 (int, int);
+long int (*selecter(int))(int, int);
---
+++ funcs.cpp
@@ -0,0 +1,6 @@
+#include <cmath>
+
+long int add (int a, int b){return a + b;}
+long int sub (int a, int b){return a - b;}
+long int mult (int a, int b){return a * b;}
+long int pow1 (int a, int b){return pow(a,b);}
